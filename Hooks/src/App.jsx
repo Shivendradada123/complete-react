4dc0e1a146d33c2b2fbb29a1593d82b69ba172e2
Shivedra123 <shivendrasaket2580@gmail.com>
2025-12-01 13:43:12 +0530

    Hooks+counter app

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,20 +1,34 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const App = () => {
-  const [first, setfirst] = useState(16);
 
-  const change = () => {
-    setfirst("Shivendra");
-  };
+const [num, num2] = useState(0)
+
+const incre=()=>{
+  num2(num+1)
+};
+const dicre=()=>{
+  num2(num-1)
+}
+const plus=()=>{
+  num2(num+5)
+}
+const rest=()=>{
+ num2(0)
+}
+
+
 
   return (
     <div>
-      value of Person = {first}
-      <div>
-        <button onClick={change}>clickme</button>
-      </div>
+<h3>counter app by Shivendra</h3>
+      <h1 >{num}</h1>
+      <button onClick={incre}>Increase</button>
+      <button onClick={dicre}>Decrease</button> <br /> 
+      <button onClick={plus}>increase 5</button>
+      <button onClick={rest}>Reset</button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
